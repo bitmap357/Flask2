ae54d8085debee99e9f6579341e19c1fca319cd3
Edmund Akogyeram <edakoyeram@gmail.com>
2023-10-16 16:34:09 +0000

    declaring of vars

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+transaction = []
 
 @app.route("/")
 def home():
-    print(request.args)
+    print(request.form.get("account"))
     return render_template("form.html")
  
- 
+
 @app.route("/mysite")
 def hello():
     return "Hello World"
