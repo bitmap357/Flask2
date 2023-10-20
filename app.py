027ef0b7e7c38fb642b35a0b59152c52288294ca
Edmund Akogyeram <edakoyeram@gmail.com>
2023-10-20 09:55:27 +0000

    showing db contents

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request
 import psycopg2
 
 app = Flask(__name__)
-transactions = []
 
 POSTGRESQL_URI = "postgres://vpadtlpg:ZxUE-nd_JZGnhoxRp2vWkwA1aKTUdbl9@surus.db.elephantsql.com/vpadtlpg"
 
@@ -30,4 +29,8 @@ def home():
 
 @app.route("/transactions")
 def show_transactions():
+    with connection:
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT * FROM transactions;")
+                transactions = cursor.fetchall()
     return render_template("transactions.jinja2", entries=transactions)
