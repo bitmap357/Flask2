2952e9cfc77314fd0648a78be4e59b391fee8398
Edmund Akogyeram <edakoyeram@gmail.com>
2023-10-20 09:45:25 +0000

    creating connection

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ transactions = []
 POSTGRESQL_URI = "postgres://vpadtlpg:ZxUE-nd_JZGnhoxRp2vWkwA1aKTUdbl9@surus.db.elephantsql.com/vpadtlpg"
 
 connection = psycopg2.connect(POSTGRESQL_URI)
+with connection:
+    
 
 @app.route("/", methods=["GET", "POST"])
 def home():
