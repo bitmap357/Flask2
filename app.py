3fbb6c0636a9abc0eef94fcd69cc797da35883cf
Edmund Akogyeram <edakoyeram@gmail.com>
2023-10-20 12:27:34 +0000

    corrections

--- app.py
+++ app.py
@@ -6,12 +6,16 @@ app = Flask(__name__)
 POSTGRESQL_URI = "postgres://vpadtlpg:ZxUE-nd_JZGnhoxRp2vWkwA1aKTUdbl9@surus.db.elephantsql.com/vpadtlpg"
 
 connection = psycopg2.connect(POSTGRESQL_URI)
-try: 
-    with connection:
-        with connection.cursor() as cursor:
-            cursor.execute("CREATE TABLE transactions (date TEXT, amount REAL, account TEXT);")
-except psycopg2.errors.DuplicateTable:
-    pass
+
+if connection.is_closed():
+    print("The connection is closed.")
+else:
+    try: 
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute("CREATE TABLE transactions (date TEXT, amount REAL, account TEXT);")
+    except psycopg2.errors.DuplicateTable:
+        pass
 
 @app.route("/", methods=["GET", "POST"])
 def home():
@@ -36,8 +40,8 @@ def home():
 
 @app.route("/transactions")
 def show_transactions():
-    with connection:
-            with connection.cursor() as cursor:
-                cursor.execute("SELECT * FROM transactions;")
-                transactions = cursor.fetchall()
+    with psycopg2.connect(POSTGRESQL_URI) as connection:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM transactions;")
+            transactions = cursor.fetchall()
     return render_template("transactions.jinja2", entries=transactions)
