cab4484be2ef7bcf5854e5180eca5a6c75081abd
Edmund Akogyeram <edakoyeram@gmail.com>
2023-10-20 09:48:27 +0000

    adding values to db

--- app.py
+++ app.py
@@ -9,12 +9,15 @@ POSTGRESQL_URI = "postgres://vpadtlpg:ZxUE-nd_JZGnhoxRp2vWkwA1aKTUdbl9@surus.db.
 connection = psycopg2.connect(POSTGRESQL_URI)
 with connection:
     with connection.cursor() as cursor:
-        
+        cursor.execute("CREATE TABLE transactions (date TEXT, amount REAL, account TEXT);")
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
         print(request.form.get("account"))
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute("")
         transactions.append(
             (
             request.form.get("date"),
