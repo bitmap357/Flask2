9fa1aec553a285971cb43017d0ed61dfb951d84f
Edmund Akogyeram <edakoyeram@gmail.com>
2023-10-20 11:56:27 +0000

    corrections

--- app.py
+++ app.py
@@ -6,9 +6,12 @@ app = Flask(__name__)
 POSTGRESQL_URI = "postgres://vpadtlpg:ZxUE-nd_JZGnhoxRp2vWkwA1aKTUdbl9@surus.db.elephantsql.com/vpadtlpg"
 
 connection = psycopg2.connect(POSTGRESQL_URI)
-with connection:
-    with connection.cursor() as cursor:
-        cursor.execute("CREATE TABLE IF NOT EXISTS transactions (date TEXT, amount REAL, account TEXT);")
+try: 
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute("CREATE TABLE transactions (date TEXT, amount REAL, account TEXT);")
+except psycopg2.errors.DuplicateTable:
+    pass
 
 @app.route("/", methods=["GET", "POST"])
 def home():
@@ -16,14 +19,18 @@ def home():
         print(request.form)
         with connection:
             with connection.cursor() as cursor:
-                cursor.execute(
-                    "INSERT INTO transactions VALUES (%s, %s, %s);", 
-                    (
-                    request.form.get("date"),
-                    float(request.form.get("amount")),
-                    request.form.get("account")
-                    )
-                            )
+                try:
+                    cursor.execute(
+                        "INSERT INTO transactions VALUES (%s, %s, %s);", 
+                        (
+                        request.form.get("date"),
+                        float(request.form.get("amount")),
+                        request.form.get("account")
+                        )
+                                )
+                    connection.commit()
+                except Exception as e:
+                   print(f"Error inserting data: {e}")
     return render_template("form.html")
  
 
