99659af27c897fa3cc063c66cca8cdf04e83b3b4
Edmund Akogyeram <edakoyeram@gmail.com>
2023-10-30 17:44:07 +0000

    corrections

--- app.py
+++ app.py
@@ -1,28 +1,27 @@
 from flask import Flask, render_template, request
-import psycopg2
+import sqlite3
 
 app = Flask(__name__)
 
-POSTGRESQL_URI = "postgres://vpadtlpg:ZxUE-nd_JZGnhoxRp2vWkwA1aKTUdbl9@surus.db.elephantsql.com/vpadtlpg"
 
-connection = psycopg2.connect(POSTGRESQL_URI)
+conn = sqlite3.connect('database.db')
 
 # if connection.is_closed():
 #     print("The connection is closed.")
 # else:
-# try: 
-with connection:
-    with connection.cursor() as cursor:
-        cursor.execute("CREATE TABLE transactions (date TEXT, amount REAL, account TEXT);")
-# except psycopg2.errors.DuplicateTable:
-#     pass
+try: 
+    with conn:
+        with conn.cursor() as cursor:
+            cursor.execute("CREATE TABLE transactions (date TEXT, amount REAL, account TEXT);")
+except sqlite3.errors.DuplicateTable:
+    pass
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
         print(request.form)
-        with connection:
-            with connection.cursor() as cursor:
+        with conn:
+            with conn.cursor() as cursor:
                 cursor.execute(
                     "INSERT INTO transactions VALUES (%s, %s, %s);", 
                     (
@@ -31,7 +30,7 @@ def home():
                     request.form.get("account")
                     )
                             )
-                connection.commit()
+                conn.commit()
                 
     return render_template("form.html")
  
