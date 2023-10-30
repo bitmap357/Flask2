605870e1ed81dfa0a5f2cd170bd4c3c30add4e95
Edmund Akogyeram <edakoyeram@gmail.com>
2023-10-30 18:51:37 +0000

    ~corrected

--- app.py
+++ app.py
@@ -1,40 +1,78 @@
+# from flask import Flask, render_template, request
+# import sqlite3
+
+# app = Flask(__name__)
+
+
+# conn = sqlite3.connect('database.db')
+
+# # if connection.is_closed():
+# #     print("The connection is closed.")
+# # else:
+
+# with conn:
+#     cursor = conn.cursor()
+#     cursor.execute("CREATE TABLE IF NOT EXISTS transactions (date TEXT, amount REAL, account TEXT);")
+    
+
+# @app.route("/", methods=["GET", "POST"])
+# def home():
+#     if request.method == "POST":
+#         print(request.form)
+#         with conn:
+#             cursor = conn.cursor()
+#             cursor.execute("INSERT INTO transactions VALUES (?, ?, ?);", (
+#                 request.form.get("date"),
+#                 float(request.form.get("amount")),
+#                 request.form.get("account")
+#             ))
+#         conn.commit()
+                
+#     return render_template("form.html")
+ 
+
+# @app.route("/transactions")
+# def show_transactions():
+#     with sqlite3.connect('database.db') as conn:
+#         cursor = conn.cursor()
+#         cursor.execute("SELECT * FROM transactions;")
+#         transactions = cursor.fetchall()
+#     return render_template("transactions.jinja2", entries=transactions)
+
+
 from flask import Flask, render_template, request
-import sqlite3
+import psycopg2
 
 app = Flask(__name__)
 
+POSTGRESQL_URI = "postgres://vpadtlpg:ZxUE-nd_JZGnhoxRp2vWkwA1aKTUdbl9@surus.db.elephantsql.com/vpadtlpg"
 
-conn = sqlite3.connect('database.db')
-
-# if connection.is_closed():
-#     print("The connection is closed.")
-# else:
-
-with conn:
-    cursor = conn.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS transactions (date TEXT, amount REAL, account TEXT);")
-    
+connection = psycopg2.connect(POSTGRESQL_URI)
+with connection:
+    with connection.cursor() as cursor:
+        cursor.execute("CREATE TABLE IF NOT EXISTS transactions (date TEXT, amount REAL, account TEXT);")
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
         print(request.form)
-        with conn:
-            cursor = conn.cursor()
-            cursor.execute("INSERT INTO transactions VALUES (?, ?, ?);", (
-                request.form.get("date"),
-                float(request.form.get("amount")),
-                request.form.get("account")
-            ))
-        conn.commit()
-                
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    "INSERT INTO transactions VALUES (%s, %s, %s);", 
+                    (
+                    request.form.get("date"),
+                    float(request.form.get("amount")),
+                    request.form.get("account")
+                    )
+                            )
     return render_template("form.html")
  
 
 @app.route("/transactions")
 def show_transactions():
-    with sqlite3.connect('database.db') as conn:
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM transactions;")
-        transactions = cursor.fetchall()
+    with connection:
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT * FROM transactions;")
+                transactions = cursor.fetchall()
     return render_template("transactions.jinja2", entries=transactions)
